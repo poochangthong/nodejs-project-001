4909c616234c5e91509f3607a8d71ae2304dd96c
poochangthong <poochangthong2@gmail.com>
2022-08-24 23:26:17 +0700

    up_onepage

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-
 const express = require('express');
 const chalk = require('chalk')
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
+
 const app = express();
 const port = 2500;
 
-app.use(morgan('combined'))
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/public/")));
 
-app.get("/",(req,res) =>{
-    res.send('Hello Teerawat node');
+app.get("/", (req, res) => {
+    res.send('No index');
 
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     // console.log("Listening on port %d",port);
-    console.log("Listening on port  " + chalk.green (port));
+    console.log("Listening on port  " + chalk.green(port));
 
 })
